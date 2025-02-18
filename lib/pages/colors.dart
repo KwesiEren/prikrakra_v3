df259677487abcc0bb3f6e9c0c5fb4563d1587b3
KwesiEren <codroidgh10@gmail.com>
2025-02-18 13:51:39 +0000

    Finished color scheme

--- lib/pages/colors.dart
+++ lib/pages/colors.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 
 @immutable
 class AppColors {
-  final color1 = const Color.fromRGBO(19, 62, 135, 1);
+  final color1 = const Color.fromARGB(255, 19, 135, 48);
   final colorw = const Color.fromARGB(255, 255, 255, 255);
   final colorbg = const Color.fromRGBO(243, 243, 224, 1);
-  final color2 = const Color.fromRGBO(96, 139, 193, 100);
+  final color2 = const Color.fromARGB(156, 96, 193, 114);
   final colortxt1 = const Color.fromARGB(255, 255, 255, 255);
   final colorbtn1 = const Color.fromARGB(255, 255, 82, 82);
-  final color3 = const Color.fromRGBO(96, 139, 193, 1);
+  final color3 = const Color.fromARGB(255, 96, 193, 115);
   final color4 = const Color.fromARGB(205, 0, 0, 0);
-  final color5 = const Color.fromARGB(208, 98, 227, 103);
+  final color5 = const Color.fromARGB(208, 94, 231, 135);
 
   const AppColors();
 }
